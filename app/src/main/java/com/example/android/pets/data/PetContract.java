d325d4838b3881be2f5d8baedc80d67a8e86d3d5
Minhaz Iftekhar <mdminhaziftekhar@gmail.com>
2022-07-06 17:23:09 +0600

    Added URI Matcher & Content Provider

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -1,9 +1,15 @@
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class PetContract {
     private PetContract(){}
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets.PetProvider";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_PETS = "pets";
+    public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
+
 
     //writing contract
     public static final class PetEntry implements BaseColumns{
